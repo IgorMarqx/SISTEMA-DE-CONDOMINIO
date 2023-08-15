e9f3eac49cf36d691cf6d752cb7e54ade33698e0
IgorMarqx <igormarquesdeazevedo11@gmail.com>
2023-08-15 10:58:05 -0300

    default template

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -3,9 +3,18 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite('resources/css/app.css')
     <title>@yield('title')</title>
 </head>
 <body>
+
+    @section('sidebar')
+
+    @endsection
+
+    <div class="container">
+        @yield('content')
+    </div>
 
 </body>
 </html>
