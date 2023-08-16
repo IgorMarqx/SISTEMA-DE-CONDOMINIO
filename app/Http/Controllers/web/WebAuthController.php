2d4d2535ffda5768ad8ad43577ccf3b8f379453f
IgorMarqx <igormarquesdeazevedo11@gmail.com>
2023-08-16 11:26:54 -0300

    creating webauthController

---
+++ app/Http/Controllers/web/WebAuthController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\web;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class WebAuthController extends Controller
+{
+    public function index()
+    {
+        return view('auth.login');
+    }
+}
