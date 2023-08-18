5e4d383a09397ba89b4d2b77fbd80b34974a1fdb
IgorMarqx <igormarquesdeazevedo11@gmail.com>
2023-08-18 07:56:01 -0300

    remove namespace

--- app/Http/Controllers/api/ApiAuthController.php
+++ app/Http/Controllers/api/ApiAuthController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers\api;
 
 use App\Http\Controllers\Controller;
-use App\Http\Requests\ApiAuthRequest;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 
