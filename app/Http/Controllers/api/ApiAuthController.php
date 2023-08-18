bb3aa166e60014130a8074065fb56682d8cf67f2
IgorMarqx <igormarquesdeazevedo11@gmail.com>
2023-08-18 12:59:37 -0300

    creating validation on login

--- app/Http/Controllers/api/ApiAuthController.php
+++ app/Http/Controllers/api/ApiAuthController.php
@@ -4,18 +4,19 @@ namespace App\Http\Controllers\api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 
 class ApiAuthController extends Controller
 {
-    public function login(Request $request)
+    public function login(Request $request): array
     {
         $array = ['error' => ''];
 
-        $validator = Validator::make($request->all(), [
-            'email' => ['required', 'email', 'unique:users'],
-            'password' => ['required', 'min:4']
-        ]);
+        $credentials = $request->only(['email', 'password']);
+
+        $validator = $this->validator($credentials);
 
         if ($validator->fails()) {
             $array['error'] = true;
@@ -23,6 +24,22 @@ class ApiAuthController extends Controller
             return $array;
         }
 
+        $email = $request->input('email');
+        $password = $request->input('password');
+
+        $token = Auth::attempt([
+            'email' => $email,
+            'password' => $password,
+        ]);
+
+        if (!$token) {
+            $array['error'] = 'E-mail ou senha inválido.';
+            return $array;
+        }
+
+        $array['token'] = $token;
+
         return $array;
     }
+
 }
