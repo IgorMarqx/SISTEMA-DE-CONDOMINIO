017b73f10a073f88a1c678dfeccaa6ff7ce0b498
IgorMarqx <igormarquesdeazevedo11@gmail.com>
2023-08-21 09:41:54 -0300

    change web routes

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [WebAuthController::class, 'index'])->name('index_login');
 
-Route::middleware('auth')->group(function () {
+// Route::middleware('auth')->group(function () {
     Route::get('/home', [WebAuthController::class, 'home'])->name('home');
-});
+// });
