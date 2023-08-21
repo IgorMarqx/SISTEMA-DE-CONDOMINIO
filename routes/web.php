58c063aea2436de8298fcb588c9bcfca6ee54180
IgorMarqx <igormarquesdeazevedo11@gmail.com>
2023-08-21 09:20:23 -0300

    test routes web

--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,7 @@ use App\Http\Controllers\web\WebAuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [WebAuthController::class, 'index'])->name('index_login');
-Route::get('home', [WebAuthController::class, 'home'])->name('home');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/home', [WebAuthController::class, 'home'])->name('home');
+});
