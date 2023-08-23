b3387f7d1d3e2d5e6f05b7ca82946405a4f0bee7
IgorMarqx <igormarquesdeazevedo11@gmail.com>
2023-08-23 18:59:08 -0300

    create new route

--- routes/api.php
+++ routes/api.php
@@ -1,4 +1,5 @@
 <?php
+
 use App\Http\Controllers\api\ApiAuthController;
 use App\Http\Controllers\user\ApiUserController;
 use Illuminate\Support\Facades\Route;
@@ -12,6 +13,7 @@ Route::post('/auth/login', [ApiAuthController::class, 'login']);
 Route::middleware('auth:api')->group(function () {
     Route::post('/auth/register', [ApiAuthController::class, 'register']);
 
+    Route::get('/users/{id}/edit', [ApiUserController::class, 'edit']);
     Route::apiResources([
         'users' => ApiUserController::class
     ]);
