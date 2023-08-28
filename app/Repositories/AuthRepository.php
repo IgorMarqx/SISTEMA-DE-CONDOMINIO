d8ff6a337e40af59825f3098a898c72248c264ca
IgorMarqx <igormarquesdeazevedo11@gmail.com>
2023-08-28 07:50:05 -0300

    implement auth repository

---
+++ app/Repositories/AuthRepository.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\User;
+use App\Repositories\Interfaces\AuthRepositoryInterface;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class AuthRepository implements AuthRepositoryInterface
+{
+    public function getToken($data): object
+    {
+        $token = Auth::attempt([
+            'email' => $data['email'],
+            'password' => $data['password'],
+        ]);
+
+        if (!$token) {
+            return response()->json(['error' => true, 'message' => 'E-mail ou senha inválidos!']);
+        }
+
+        return response()->json(['token' => $token]);
+    }
+
+    public function registerUser($data): object
+    {
+        $user = User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => Hash::make($data['password']),
+        ]);
+
+        if ($user) {
+            return response()->json(['message' => 'Cadastrado com sucesso.']);
+        }
+
+        return response()->json(['error' => true]);
+    }
+}
