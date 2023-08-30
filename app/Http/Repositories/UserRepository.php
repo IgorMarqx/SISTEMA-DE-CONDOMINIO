0f2f7fb5babdd88e1791b4a2f6e045c07f5b98d3
IgorMarqx <igormarquesdeazevedo11@gmail.com>
2023-08-30 07:41:26 -0300

    create user Repository

---
+++ app/Http/Repositories/UserRepository.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\User;
+
+class UserRepository
+{
+    protected $user;
+
+    public function __construct(User $user)
+    {
+        $this->user = $user;
+    }
+
+    public function getUsers()
+    {
+        return $this->user->all();
+    }
+}
