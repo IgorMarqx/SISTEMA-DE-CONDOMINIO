84cb5d4149be9d9cf20c02484fcb625a96952cc0
IgorMarqx <igormarquesdeazevedo11@gmail.com>
2023-09-04 16:25:16 -0300

    create dashboard migration

--- database/migrations/2023_08_29_130052_create_dashboards_table.php
+++ database/migrations/2023_08_29_130052_create_dashboards_table.php
@@ -25,7 +25,7 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('dashboard', function (Blueprint $table) {
+        Schema::table('dashboards', function (Blueprint $table) {
             $table->dropForeign(['condominium_id']);
         });
 
