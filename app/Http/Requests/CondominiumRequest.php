6f712dfab2ff93c412caf4ffbdcd03a289f2d9a3
IgorMarqx <igormarquesdeazevedo11@gmail.com>
2023-09-06 20:19:00 -0300

    implement CondominiumRequest.php

--- app/Http/Requests/CondominiumRequest.php
+++ app/Http/Requests/CondominiumRequest.php
@@ -3,26 +3,24 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Contracts\Validation\Validator;
 
 class CondominiumRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     */
-    public function authorize(): bool
-    {
-        return false;
-    }
-
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
-     */
     public function rules(): array
     {
         return [
-            //
+            'name' => 'required|unique:condominiums',
+            'address' => 'required|unique:condominiums'
         ];
+    }
+
+    public function failedValidation(Validator $validator): object
+    {
+        throw new HttpResponseException(response()->json([
+            'error' => true,
+            'message' => $validator->errors()->first()
+        ]));
     }
 }
