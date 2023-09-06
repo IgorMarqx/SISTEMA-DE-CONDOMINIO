97eb4f28ffb30bddb7398d9deed85b807f63fa75
IgorMarqx <igormarquesdeazevedo11@gmail.com>
2023-09-05 21:39:51 -0300

    implement parameters on funcion

--- app/Repositories/Interfaces/CondominiumRepositoryInterface.php
+++ app/Repositories/Interfaces/CondominiumRepositoryInterface.php
@@ -5,8 +5,8 @@ namespace App\Repositories\Interfaces;
 interface CondominiumRepositoryInterface
 {
     public function getAll(): object;
-    public function storeCondominium(): void;
-    public function findCondominiumById();
+    public function storeCondominium($data): void;
+    public function findCondominiumById($id): object;
     public function updateCondominium(): void;
     public function deleteCondominium(): void;
 }
