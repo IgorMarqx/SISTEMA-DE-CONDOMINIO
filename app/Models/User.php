958be9151c9ebd12e3f783675117cbe4ef366554
IgorMarqx <igormarquesdeazevedo11@gmail.com>
2023-09-05 21:21:56 -0300

    add new inputs

--- app/Models/User.php
+++ app/Models/User.php
@@ -22,6 +22,8 @@ class User extends Authenticatable implements JWTSubject
         'name',
         'email',
         'password',
+        'condominium_id',
+        'apartment_id',
     ];
 
     /**
