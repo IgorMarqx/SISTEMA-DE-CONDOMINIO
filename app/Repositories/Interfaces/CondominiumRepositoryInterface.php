06e1c0e85bfdf5e29bcdef0790006255a3c76414
IgorMarqx <igormarquesdeazevedo11@gmail.com>
2023-09-06 20:18:36 -0300

    implement CondominiumRepositoryInterface

--- app/Repositories/Interfaces/CondominiumRepositoryInterface.php
+++ app/Repositories/Interfaces/CondominiumRepositoryInterface.php
@@ -5,8 +5,8 @@ namespace App\Repositories\Interfaces;
 interface CondominiumRepositoryInterface
 {
     public function getAll(): object;
-    public function storeCondominium($data): void;
+    public function storeCondominium($data): object;
     public function findCondominiumById($id): object;
-    public function updateCondominium(): void;
-    public function deleteCondominium(): void;
+    public function updateCondominium($data, $id): object;
+    public function deleteCondominium($id): object;
 }
