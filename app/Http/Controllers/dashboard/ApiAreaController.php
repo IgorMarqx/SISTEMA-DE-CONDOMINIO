525001b9dd03f9484332efb8a5f8addb9cef0671
IgorMarqx <igormarquesdeazevedo11@gmail.com>
2023-09-06 22:28:12 -0300

    implemente area controller

--- app/Http/Controllers/dashboard/ApiAreaController.php
+++ app/Http/Controllers/dashboard/ApiAreaController.php
@@ -3,16 +3,25 @@
 namespace App\Http\Controllers\dashboard;
 
 use App\Http\Controllers\Controller;
+use App\Repositories\Interfaces\AreaRepositoryInterface;
 use Illuminate\Http\Request;
 
 class ApiAreaController extends Controller
 {
+    private $areaRepository;
+
+    public function __construct(AreaRepositoryInterface $areaRepository)
+    {
+        $this->areaRepository = $areaRepository;
+        $this->middleware('api.auth');
+    }
+
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(): object
     {
-        //
+        return $this->areaRepository->getAll();
     }
 
     /**
