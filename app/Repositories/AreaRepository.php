c5c3f6ec811667defb4e8c803f73bd9234cca95e
IgorMarqx <igormarquesdeazevedo11@gmail.com>
2023-09-07 15:24:03 -0300

    implement area repository

--- app/Repositories/AreaRepository.php
+++ app/Repositories/AreaRepository.php
@@ -9,15 +9,57 @@ class AreaRepository implements AreaRepositoryInterface
 {
     public function getAll(): object
     {
-        $area = Area::all();
-
-        return $area;
+        return Area::all();
     }
 
     public function storeArea($data): object
     {
         $area = Area::create([
-
+            'name' => $data['name'],
+            'condominium_id' => $data['condominium_id'],
+            'operations_id' => $data['operations_id'],
         ]);
+        $area->save();
+
+        return response()->json(['error' => '', 'message' => 'Área cadastrada com sucesso.'], 201);
     }
+
+    public function findAreaById($id): object
+    {
+        $area = Area::find($id);
+
+        if (!$area) {
+            return response()->json(['error' => true, 'message' => 'Área não encontrada'], 404);
+        }
+
+        return $area;
+    }
+
+    public function updateArea($data, $id): object
+    {
+        $area = Area::find($id);
+
+        if ($area) {
+            $area->name = $data['name'];
+            $area->condominium_id = $data['condominium_id'];
+            $area->operations_id = $data['operations_id'];
+            $area->save();
+
+            return response()->json(['error' => '', 'message' => 'Área atualizada com sucesso.'], 201);
+        }
+
+        return response()->json(['error' => true, 'message' => 'Área não encontrada.'], 404);
+    }
+
+    public function deleteArea($id): object
+    {
+        $area = Area::find($id);
+
+        if (!$area) {
+            return response()->json(['error' => true, 'message' => 'Area não encontrada.'], 404);
+        }
+
+        return response()->json($area);
+    }
+
 }
