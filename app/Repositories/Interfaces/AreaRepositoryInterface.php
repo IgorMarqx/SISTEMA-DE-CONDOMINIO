1541325e6036dfa49563c43076a556ce9641c0ce
IgorMarqx <igormarquesdeazevedo11@gmail.com>
2023-09-07 15:24:12 -0300

    implement area repository interface

--- app/Repositories/Interfaces/AreaRepositoryInterface.php
+++ app/Repositories/Interfaces/AreaRepositoryInterface.php
@@ -6,6 +6,7 @@ interface AreaRepositoryInterface
 {
     public function getAll(): object;
     public function storeArea($data): object;
+    public function findAreaById($id): object;
     public function updateArea($data, $id): object;
     public function deleteArea($id): object;
 }
