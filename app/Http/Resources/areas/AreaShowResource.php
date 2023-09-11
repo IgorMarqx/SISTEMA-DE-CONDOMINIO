42f5aeaa73b9a048175cb5a455ade2d630204ea4
IgorMarqx <igormarquesdeazevedo11@gmail.com>
2023-09-11 18:28:01 -0300

    implement area show resource

---
+++ app/Http/Resources/areas/AreaShowResource.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Resources\areas;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class AreaShowResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'allowed' => $this->allowed,
+            'name' => $this->name,
+            'days' => $this->days,
+            'start_time' => $this->start_time,
+            'end_time' => $this->end_time,
+            'condominium_id' => $this->condominium_id,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+            'condominium' => $this->condominium
+        ];
+    }
+}
