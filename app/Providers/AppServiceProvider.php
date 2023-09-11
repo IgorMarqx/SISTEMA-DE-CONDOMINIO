bdb0377f0cd0b5725c67e41f0e4baefb7d875037
IgorMarqx <igormarquesdeazevedo11@gmail.com>
2023-09-11 18:26:14 -0300

    change bind to singleton in register method

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Models\Condominium;
 use App\Repositories\AreaRepository;
 use App\Repositories\Interfaces\AreaRepositoryInterface;
 use App\Repositories\Interfaces\AuthRepositoryInterface;
@@ -10,6 +11,7 @@ use App\Repositories\UserRepository;
 use App\Repositories\AuthRepository;
 use App\Repositories\CondominiumRepository;
 use App\Repositories\Interfaces\CondominiumRepositoryInterface;
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
 
@@ -20,10 +22,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        $this->app->bind(UserRepositoryInterface::class, UserRepository::class);
-        $this->app->bind(AuthRepositoryInterface::class, AuthRepository::class);
-        $this->app->bind(CondominiumRepositoryInterface::class, CondominiumRepository::class);
-        $this->app->bind(AreaRepositoryInterface::class, AreaRepository::class);
+        $this->app->singleton(UserRepositoryInterface::class, UserRepository::class);
+        $this->app->singleton(AuthRepositoryInterface::class, AuthRepository::class);
+        $this->app->singleton(CondominiumRepositoryInterface::class, CondominiumRepository::class);
+        $this->app->singleton(AreaRepositoryInterface::class, AreaRepository::class);
     }
 
     /**
@@ -32,5 +34,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Schema::defaultStringLength(191);
+
+        Validator::extend('condominium_exists', function($attribute, $value){
+            return Condominium::where('id', $value)->exists();
+        });
     }
 }
