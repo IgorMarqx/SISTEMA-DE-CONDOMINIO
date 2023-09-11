1f687f0a23ac27680ddb3ce72ceabc8506997ea2
IgorMarqx <igormarquesdeazevedo11@gmail.com>
2023-09-11 18:27:32 -0300

    change the reutnr on area repository interface

--- app/Repositories/Interfaces/AreaRepositoryInterface.php
+++ app/Repositories/Interfaces/AreaRepositoryInterface.php
@@ -2,10 +2,12 @@
 
 namespace App\Repositories\Interfaces;
 
+use App\Models\Area;
+
 interface AreaRepositoryInterface
 {
     public function getAll(): object;
-    public function storeArea($data): object;
+    public function storeArea($data): Area;
     public function findAreaById($id): object;
     public function updateArea($data, $id): object;
     public function deleteArea($id): object;
