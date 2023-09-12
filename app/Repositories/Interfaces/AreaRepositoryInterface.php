fbe4c6cb93e9280bd966f0089746322ffda69a0b
IgorMarqx <igormarquesdeazevedo11@gmail.com>
2023-09-12 20:54:02 -0300

    implement area repository interface

--- app/Repositories/Interfaces/AreaRepositoryInterface.php
+++ app/Repositories/Interfaces/AreaRepositoryInterface.php
@@ -2,13 +2,17 @@
 
 namespace App\Repositories\Interfaces;
 
+use App\Http\Resources\areas\AreaErrorResource;
+use App\Http\Resources\areas\AreaShowResource;
+use App\Http\Resources\areas\AreaUpdateResource;
 use App\Models\Area;
+use Illuminate\Database\Eloquent\Collection;
 
 interface AreaRepositoryInterface
 {
-    public function getAll(): object;
+    public function getAll(): Collection;
     public function storeArea($data): Area;
-    public function findAreaById($id): object;
-    public function updateArea($data, $id): object;
+    public function findAreaById($id): AreaErrorResource|AreaShowResource;
+    public function updateArea($data, $id): AreaUpdateResource|AreaErrorResource;
     public function deleteArea($id): object;
 }
