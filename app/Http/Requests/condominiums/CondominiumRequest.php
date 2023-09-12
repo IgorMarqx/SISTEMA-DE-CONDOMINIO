2a0d760a69d819417b564a395f5500f2e2c2d127
IgorMarqx <igormarquesdeazevedo11@gmail.com>
2023-09-12 20:54:58 -0300

    change the method name

--- app/Http/Requests/condominiums/CondominiumRequest.php
+++ app/Http/Requests/condominiums/CondominiumRequest.php
@@ -1,10 +1,10 @@
 <?php
 
-namespace App\Http\Requests;
+namespace App\Http\Requests\condominiums;
 
+use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Http\Exceptions\HttpResponseException;
-use Illuminate\Contracts\Validation\Validator;
 
 class CondominiumRequest extends FormRequest
 {
