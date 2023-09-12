937ab74197174bda4cc54bb768113d6e6a6e7f81
IgorMarqx <igormarquesdeazevedo11@gmail.com>
2023-09-12 20:49:20 -0300

    implement relationship in apartment table

--- database/migrations/2023_09_05_174650_create_apartments_table.php
+++ database/migrations/2023_09_05_174650_create_apartments_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,8 +12,11 @@ return new class extends Migration
     {
         Schema::create('apartments', function (Blueprint $table) {
             $table->id();
-            $table->integer('number');
+            $table->string('identify');
+            $table->unsignedBigInteger('condominium_id');
             $table->timestamps();
+
+            $table->foreign('condominium_id')->references('id')->on('condominiums')->cascadeOnDelete();
         });
     }
 
