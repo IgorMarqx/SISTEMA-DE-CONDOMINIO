62b439c9a8762381ad14f7bd0d4d655bddd8d545
IgorMarqx <igormarquesdeazevedo11@gmail.com>
2023-09-13 20:18:17 -0300

    fix the methos in condominium repository interface

--- app/Repositories/Interfaces/CondominiumRepositoryInterface.php
+++ app/Repositories/Interfaces/CondominiumRepositoryInterface.php
@@ -2,11 +2,15 @@
 
 namespace App\Repositories\Interfaces;
 
+use App\Http\Resources\ApiResource;
+use App\Http\Resources\condominiums\CondominiumShowResource;
+use Illuminate\Database\Eloquent\Collection;
+
 interface CondominiumRepositoryInterface
 {
-    public function getAll(): object;
-    public function storeCondominium($data): object;
-    public function findCondominiumById($id): object;
-    public function updateCondominium($data, $id): object;
-    public function deleteCondominium($id): object;
+    public function getAll(): Collection;
+    public function storeCondominium($data): ApiResource;
+    public function findCondominiumById($id): CondominiumShowResource|ApiResource;
+    public function updateCondominium($data, $id): ApiResource;
+    public function deleteCondominium($id): ApiResource;
 }
