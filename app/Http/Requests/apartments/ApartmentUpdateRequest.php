6487054c41dccf95ff89419a10f1fc0ac5dd8f15
IgorMarqx <igormarquesdeazevedo11@gmail.com>
2023-09-13 17:40:11 -0300

    create apartment request

---
+++ app/Http/Requests/apartments/ApartmentUpdateRequest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Requests\apartments;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class ApartmentUpdateRequest extends FormRequest
+{
+    public function rules(): array
+    {
+        return [
+            'identify' => 'required',
+        ];
+    }
+
+    public function failedValidation(Validator $validator)
+    {
+        throw  new HttpResponseException(response()->json([
+            'error' => true,
+            'message' => $validator->errors()->first()
+        ], 402));
+    }
+}
