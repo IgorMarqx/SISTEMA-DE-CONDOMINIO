da2bd8e2eb46a241b132c0fea1c4136baa2b6a12
IgorMarqx <igormarquesdeazevedo11@gmail.com>
2023-09-13 17:40:38 -0300

    fix the methods and imports on apartment controller

--- app/Http/Controllers/dashboard/ApiApartmentController.php
+++ app/Http/Controllers/dashboard/ApiApartmentController.php
@@ -4,12 +4,10 @@ namespace App\Http\Controllers\dashboard;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\apartments\ApartmentCreateRequest;
-use App\Http\Resources\apartments\ApartmentErroResource;
-use App\Http\Resources\apartments\ApartmentResource;
-use App\Http\Resources\apartments\ApartmentShowResource;
+use App\Http\Requests\apartments\ApartmentUpdateRequest;
+use App\Http\Resources\ApiResource;
 use App\Repositories\ApartmentRepository;
 use Illuminate\Http\Request;
-use \Illuminate\Database\Eloquent\Collection;
 
 class ApiApartmentController extends Controller
 {
@@ -33,35 +31,35 @@ class ApiApartmentController extends Controller
      * Store a newly created resource in storage.
      * @throws \Exception
      */
-    public function store(ApartmentCreateRequest $request): ApartmentResource
+    public function store(ApartmentCreateRequest $request): ApiResource
     {
-        $apartment = $this->apartmentRepository->storeApartment($request);
-
-        return new ApartmentResource($apartment);
+        return $this->apartmentRepository->storeApartment($request);
     }
 
     /**
      * Display the specified resource.
      * @throws \Exception
      */
-    public function show(string $id): ApartmentErroResource|ApartmentShowResource
+    public function show(string $id): ApiResource
     {
         return $this->apartmentRepository->findApartmentById($id);
     }
 
     /**
      * Update the specified resource in storage.
+     * @throws \Exception
      */
-    public function update(Request $request, string $id)
+    public function update(ApartmentUpdateRequest $request, string $id): ApiResource
     {
-        //
+        return $this->apartmentRepository->updateApartment($request, $id);
     }
 
     /**
      * Remove the specified resource from storage.
+     * @throws \Exception
      */
-    public function destroy(string $id)
+    public function destroy(string $id): ApiResource
     {
-        //
+        return $this->apartmentRepository->deleteApartment($id);
     }
 }
