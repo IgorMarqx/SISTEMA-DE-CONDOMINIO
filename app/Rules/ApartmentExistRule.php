3854b61ed3ed80f41c23dad175d6031a40799cb2
IgorMarqx <igormarquesdeazevedo11@gmail.com>
2023-09-14 20:05:19 -0300

    implement rule

---
+++ app/Rules/ApartmentExistRule.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Rules;
+
+use App\Models\Apartment;
+use App\Models\Condominium;
+use Closure;
+use Illuminate\Contracts\Validation\ValidationRule;
+
+class ApartmentExistRule implements ValidationRule
+{
+    private int $apartment_id;
+
+    public function __construct(int $apartment_id)
+    {
+        $this->apartment_id = $apartment_id;
+    }
+
+    public function validate(string $attribute, mixed $value, Closure $fail): void
+    {
+        $apartment = Apartment::where('id', $this->apartment_id)->exists();
+
+        if (!$apartment) {
+            $fail('Condominio informado não existe.');
+        }
+    }
+}
