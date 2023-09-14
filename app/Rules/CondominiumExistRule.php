aa00f1769abde7c82a0ae077af3ef6a6f64b6913
IgorMarqx <igormarquesdeazevedo11@gmail.com>
2023-09-14 20:05:25 -0300

    implement rule

---
+++ app/Rules/CondominiumExistRule.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Rules;
+
+use App\Models\Condominium;
+use Closure;
+use Illuminate\Contracts\Validation\ValidationRule;
+
+class CondominiumExistRule implements ValidationRule
+{
+    private int $condominium_id;
+
+    public function __construct(int $condominium_id)
+    {
+        $this->condominium_id = $condominium_id;
+    }
+
+    /**
+     * Run the validation rule.
+     *
+     * @param \Closure(string): \Illuminate\Translation\PotentiallyTranslatedString $fail
+     */
+    public function validate(string $attribute, mixed $value, Closure $fail): void
+    {
+        $condominium = Condominium::where('id', $this->condominium_id)->exists();
+
+        if (!$condominium) {
+            $fail('Condominio informado não existe.');
+        }
+    }
+}
