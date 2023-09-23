77a0aa0bc86a317595c3a0cc4e29234021644560
IgorMarqx <igormarquesdeazevedo11@gmail.com>
2023-09-23 13:13:12 -0300

    remove column apartment_id

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,12 +18,10 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->unsignedBigInteger('condominium_id');
-            $table->unsignedBigInteger('apartment_id');
             $table->rememberToken();
             $table->timestamps();
 
             $table->foreign('condominium_id')->references('id')->on('condominiums');
-            $table->foreign('apartment_id')->references('id')->on('apartments');
         });
     }
 
