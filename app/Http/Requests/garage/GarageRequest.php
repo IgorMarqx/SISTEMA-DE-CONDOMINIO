03195c2ee1bbad9cebe684789a10531cb9b9ed76
IgorMarqx <igormarquesdeazevedo11@gmail.com>
2023-09-24 00:04:18 -0300

    implement garage request

---
+++ app/Http/Requests/garage/GarageRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests\garage;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Http\JsonResponse;
+
+class GarageRequest extends FormRequest
+{
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'identify' => ['required', 'string'],
+            'apartment_id' => ['required', 'numeric', 'apartment_exists'],
+        ];
+    }
+
+    public function failedValidation(Validator $validator): JsonResponse
+    {
+        throw new HttpResponseException(response()->json([
+            'error' => true,
+            'message' => $validator->errors()->first()
+        ]));
+    }
+}
