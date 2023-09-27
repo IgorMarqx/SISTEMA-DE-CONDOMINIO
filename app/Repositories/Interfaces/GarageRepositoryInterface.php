9d92aee73c2a8496367ce76b0df69710ef8bd92c
IgorMarqx <igormarquesdeazevedo11@gmail.com>
2023-09-26 21:23:00 -0300

    change the methods returns

--- app/Repositories/Interfaces/GarageRepositoryInterface.php
+++ app/Repositories/Interfaces/GarageRepositoryInterface.php
@@ -3,11 +3,12 @@
 namespace App\Repositories\Interfaces;
 
 use App\Models\Garage;
+use Illuminate\Database\Eloquent\Collection;
 
 interface GarageRepositoryInterface
 {
     public function storeGarage($data): Garage;
-    public function findGarageById($id): Garage;
-    public function updateGarage($id, $data): Garage;
-    public function deleteGarage($id): Garage;
+    public function findGarageById($id): Garage|null|Collection;
+    public function updateGarage(Garage $garage, $data): Garage|bool|null;
+    public function deleteGarage(Garage $garage): Garage|bool|null;
 }
