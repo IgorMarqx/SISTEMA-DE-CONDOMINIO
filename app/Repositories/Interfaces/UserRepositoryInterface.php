bb6f5b80bfe358a25a8ba4636a9e22520c1cbb75
IgorMarqx <igormarquesdeazevedo11@gmail.com>
2023-09-26 21:23:33 -0300

    fix the methods returns

--- app/Repositories/Interfaces/UserRepositoryInterface.php
+++ app/Repositories/Interfaces/UserRepositoryInterface.php
@@ -4,13 +4,14 @@ namespace App\Repositories\Interfaces;
 
 use App\Http\Resources\ApiResource;
 use App\Http\Resources\user\UserShowResource;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Collection;
 
 interface UserRepositoryInterface
 {
     public function getAll(): Collection;
     public function storeUser($data): ApiResource;
-    public function findUserById($id): ApiResource|UserShowResource;
-    public function updateUser($data, $id): ApiResource;
-    public function destroyUser($id): ApiResource;
+    public function findUserById($id): User;
+    public function updateUser($data, $id): bool;
+    public function destroyUser($id): User;
 }
