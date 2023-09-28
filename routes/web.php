541647af57d838a69f6d8245f5c24e163efdf6bf
IgorMarqx <igormarquesdeazevedo11@gmail.com>
2023-09-28 15:35:41 -0300

    remove: delete the web routes

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,2 @@
 <?php
 
-use App\Http\Controllers\web\WebAuthController;
-use Illuminate\Support\Facades\Route;
-
-Route::get('/', [WebAuthController::class, 'index'])->name('index_login');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/home', [WebAuthController::class, 'home'])->name('home');
-});
