948f73eac743a717915009b7e18075bcae2ee606
IgorMarqx <igormarquesdeazevedo11@gmail.com>
2023-09-28 12:07:30 -0300

    feat: implemented new returns in methods and removing the dependency injection

--- app/Repositories/Interfaces/UserRepositoryInterface.php
+++ app/Repositories/Interfaces/UserRepositoryInterface.php
@@ -10,8 +10,12 @@ use Illuminate\Database\Eloquent\Collection;
 interface UserRepositoryInterface
 {
     public function getAll(): Collection;
+
     public function storeUser($data): ApiResource;
-    public function findUserById($id): User;
-    public function updateUser($data, $id): bool;
-    public function destroyUser($id): User;
+
+    public function findUserById($id): User|null|Collection;
+
+    public function updateUser(User $user, $data): User|bool|null;
+
+    public function destroyUser(User $user): User|null|bool;
 }
