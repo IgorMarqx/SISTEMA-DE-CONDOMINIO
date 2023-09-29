cf9c3a13a392e0f37caa10e78b7c7103e9f21c72
IgorMarqx <igormarquesdeazevedo11@gmail.com>
2023-09-29 14:42:51 -0300

    refactor: change all the methods and returns

--- app/Http/Controllers/dashboard/ApiAreaController.php
+++ app/Http/Controllers/dashboard/ApiAreaController.php
@@ -7,56 +7,101 @@ use App\Http\Requests\areas\AreaCreateRequest;
 use App\Http\Requests\areas\AreaUpdateRequest;
 use App\Http\Resources\ApiResource;
 use App\Http\Resources\areas\AreaShowResource;
-use App\Repositories\Interfaces\AreaRepositoryInterface;
+use App\Services\area\AreaService;
+use Exception;
 use \Illuminate\Database\Eloquent\Collection;
 
 class ApiAreaController extends Controller
 {
-    private AreaRepositoryInterface $areaRepository;
+    private AreaService $areaService;
 
-    public function __construct(AreaRepositoryInterface $areaRepository)
+    public function __construct(AreaService $areaService)
     {
-        $this->areaRepository = $areaRepository;
+        $this->areaService = $areaService;
         $this->middleware('api.auth');
     }
 
     /**
      * Display a listing of the resource.
+     * @throws Exception
      */
     public function index(): Collection
     {
-        return $this->areaRepository->getAll();
+        try {
+            return $this->areaService->getAll();
+        } catch (Exception $e) {
+            throw new Exception('Erro ao listar apartamentos:' . $e->getMessage());
+        }
     }
 
     /**
      * Store a newly created resource in storage.
+     * @throws Exception
      */
     public function store(AreaCreateRequest $request): ApiResource
     {
-        return $this->areaRepository->storeArea($request);
+        $this->areaService->storeArea($request);
+
+        try {
+            return new ApiResource(['error' => false, 'message' => 'Área criada com sucesso'], 201);
+        } catch (Exception $e) {
+            throw new Exception('Falha ao criar area:' . $e->getMessage());
+        }
     }
 
     /**
      * Display the specified resource.
+     * @throws Exception
      */
     public function show(string $id): ApiResource|AreaShowResource
     {
-        return $this->areaRepository->findAreaById($id);
+        $area = $this->areaService->findAreaById($id);
+
+        if (!$area) {
+            return new ApiResource(['error' => true, 'message' => 'Área não encontrada'], 404);
+        }
+        try {
+            return new AreaShowResource($area);
+        } catch (Exception $e) {
+            throw new Exception('Erro ao criar área:' . $e->getMessage());
+        }
     }
 
     /**
      * Update the specified resource in storage.
+     * @throws Exception
      */
     public function update(AreaUpdateRequest $request, string $id): ApiResource
     {
-        return $this->areaRepository->updateArea($request, $id);
+        $area = $this->areaService->updateArea($request, $id);
+
+        if (!$area) {
+            return new ApiResource(['error' => true, 'message' => 'Área não encontrada'], 404);
+        }
+        try {
+            return new ApiResource(['error' => false, 'message' => 'Área atualizada com sucesso'], 200);
+        } catch
+        (Exception $e) {
+            throw new Exception('Erro ao criar apartamento:' . $e->getMessage());
+        }
     }
 
     /**
      * Remove the specified resource from storage.
+     * @throws Exception
      */
     public function destroy(string $id): ApiResource
     {
-        return $this->areaRepository->deleteArea($id);
+        $area = $this->areaService->deleteArea($id);
+
+        if (!$area) {
+            return new ApiResource(['error' => true, 'message' => 'Área não encontrada.'], 404);
+        }
+
+        try {
+            return new ApiResource(['error' => false, 'message' => 'Área deletada com sucesso.'], 200);
+        } catch (Exception $e) {
+            throw new Exception('Erro ao criar apartamento:' . $e->getMessage());
+        }
     }
 }
