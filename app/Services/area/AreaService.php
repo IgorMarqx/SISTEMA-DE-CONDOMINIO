04e18b33950834a7e7c5dc8df01367e1472c36a9
IgorMarqx <igormarquesdeazevedo11@gmail.com>
2023-09-29 14:45:33 -0300

    feat: implement area service

---
+++ app/Services/area/AreaService.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Services\area;
+
+use App\Models\Area;
+use App\Repositories\Interfaces\AreaRepositoryInterface;
+use Illuminate\Database\Eloquent\Collection;
+
+class AreaService
+{
+    private AreaRepositoryInterface $areaRepository;
+
+    public function __construct(AreaRepositoryInterface $areaRepository)
+    {
+        $this->areaRepository = $areaRepository;
+    }
+
+    public function getAll(): Collection
+    {
+        return $this->areaRepository->getAll();
+    }
+
+    public function storeArea($data): Area
+    {
+        return $this->areaRepository->storeArea($data);
+    }
+
+    public function findAreaById($id): Collection|Area|null
+    {
+        return $this->areaRepository->findAreaById($id);
+    }
+
+    public function updateArea($data, $id): bool
+    {
+        $area = $this->areaRepository->findAreaById($id);
+
+        if (!$area) {
+            return false;
+        }
+
+        return $this->areaRepository->updateArea($area, [
+            'name' => $data->name,
+            'days' => $data->days,
+            'start_time' => $data->start_time,
+            'end_time' => $data->end_time,
+            'condominium_id' => $data->condominium_id,
+            'allowed' => $data->allowed
+        ]);
+    }
+
+    public function deleteArea($id): null|bool
+    {
+        $area = $this->areaRepository->findAreaById($id);
+
+        if (!$area) {
+            return null;
+        }
+
+        return $this->areaRepository->deleteArea($area);
+    }
+
+}
