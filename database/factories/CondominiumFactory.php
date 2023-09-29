1af716953595be68aec654fc728eb4832b34e6f0
IgorMarqx <igormarquesdeazevedo11@gmail.com>
2023-09-29 14:41:13 -0300

    feat: implement factory for testing api

---
+++ database/factories/CondominiumFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Condominium>
+ */
+class CondominiumFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->name(),
+            'address' => fake()->address(),
+            'color' => fake()->hexColor(),
+            'created_at' => now(),
+            'updated_at' => now(),
+        ];
+    }
+}
