cbd611119e3af796d93b8df7f1201ae3f1e9950b
IgorMarqx <igormarquesdeazevedo11@gmail.com>
2023-09-29 09:02:52 -0300

    feat: change methods returns

--- app/Repositories/Interfaces/CondominiumRepositoryInterface.php
+++ app/Repositories/Interfaces/CondominiumRepositoryInterface.php
@@ -4,13 +4,18 @@ namespace App\Repositories\Interfaces;
 
 use App\Http\Resources\ApiResource;
 use App\Http\Resources\condominiums\CondominiumShowResource;
+use App\Models\Condominium;
 use Illuminate\Database\Eloquent\Collection;
 
 interface CondominiumRepositoryInterface
 {
     public function getAll(): Collection;
-    public function storeCondominium($data): ApiResource;
-    public function findCondominiumById($id): CondominiumShowResource|ApiResource;
-    public function updateCondominium($data, $id): ApiResource;
-    public function deleteCondominium($id): ApiResource;
+
+    public function storeCondominium($data): Condominium;
+
+    public function findCondominiumById($id): Condominium|Collection|null;
+
+    public function updateCondominium(Condominium $condominium, $data): Condominium|null|bool;
+
+    public function deleteCondominium(Condominium $condominium): bool;
 }
