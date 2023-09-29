9d47d32a2c4c102df3a96ae7c77232571189e1ce
IgorMarqx <igormarquesdeazevedo11@gmail.com>
2023-09-29 14:41:20 -0300

    feat: implement factory for testing api

---
+++ database/factories/GarageFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Apartment;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Garage>
+ */
+class GarageFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        $apartment = Apartment::factory()->create();
+
+        return [
+            'identify' => 'GARAGE ' . fake()->numberBetween(1, 999),
+            'apartment_id' => $apartment->id,
+        ];
+    }
+}
