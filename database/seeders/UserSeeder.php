5408b396ea8ea5d4910217f29a739d3249f4b641
IgorMarqx <igormarquesdeazevedo11@gmail.com>
2023-11-16 20:36:56 -0300

    config: using pint

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,7 +2,6 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
@@ -20,7 +19,7 @@ class UserSeeder extends Seeder
             'password' => Hash::make('12345'),
             'condominium_id' => '1',
             'created_at' => date('Y-m-d H:i:s'),
-            'updated_at' => date('Y-m-d H:i:s')
+            'updated_at' => date('Y-m-d H:i:s'),
         ]);
     }
 }
