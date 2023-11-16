7a405964b2ec073e29a7cde3774d27d32b71d285
IgorMarqx <igormarquesdeazevedo11@gmail.com>
2023-11-16 20:34:10 -0300

    config: using pint

--- app/Http/Controllers/user/ApiUserController.php
+++ app/Http/Controllers/user/ApiUserController.php
@@ -3,16 +3,14 @@
 namespace App\Http\Controllers\user;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\UserRequest;
 use App\Http\Resources\ApiResource;
 use App\Http\Resources\user\UserShowResource;
-use App\Http\Requests\UserRequest;
 use App\Services\user\UserService;
 use Exception;
-use Illuminate\Database\Eloquent\Collection;
 
 class ApiUserController extends Controller
 {
-
     private UserService $userService;
 
     public function __construct(UserService $userService)
@@ -24,13 +22,14 @@ class ApiUserController extends Controller
     /**
      * @throws \Exception
      */
-    public function index(): Collection
+    public function index(): \Illuminate\Support\Collection
     {
         return $this->userService->getAll();
     }
 
     /**
      * Display the specified resource.
+     *
      * @throws Exception
      */
     public function show(string $id): ApiResource|UserShowResource
@@ -44,45 +43,47 @@ class ApiUserController extends Controller
         try {
             return new UserShowResource($user);
         } catch (Exception $e) {
-            throw new Exception('Erro: ' . $e->getMessage());
+            throw new Exception('Erro: '.$e->getMessage());
         }
     }
 
     /**
      * Update the specified resource in storage.
+     *
      * @throws Exception
      */
     public function update(UserRequest $request, string $id): ApiResource
     {
         $user = $this->userService->updateUser($request, $id);
 
-        if (!$user) {
+        if (! $user) {
             return new ApiResource(['error' => true, 'message' => 'Usuário não encontrado'], 404);
         }
 
         try {
             return new ApiResource(['error' => false, 'message' => 'Usuário atualizado com sucesso'], 200);
         } catch (Exception $e) {
-            throw  new Exception('Erro: ' . $e->getMessage());
+            throw new Exception('Erro: '.$e->getMessage());
         }
     }
 
     /**
      * Remove the specified resource from storage.
+     *
      * @throws Exception
      */
     public function destroy(string $id): ApiResource
     {
         $user = $this->userService->destroyUser($id);
 
-        if (!$user) {
+        if (! $user) {
             return new ApiResource(['error' => true, 'message' => 'Usuário não encontrado'], 404);
         }
 
         try {
             return new ApiResource(['error' => false, 'message' => 'Usuário deletado com sucesso'], 200);
         } catch (Exception $e) {
-            throw new Exception('Erro: ' . $e->getMessage());
+            throw new Exception('Erro: '.$e->getMessage());
         }
     }
 }
