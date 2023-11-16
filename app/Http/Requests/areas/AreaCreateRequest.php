791ee9aaff953b1bd0c4fe82e6e7ef4e2d33790c
IgorMarqx <igormarquesdeazevedo11@gmail.com>
2023-11-16 20:34:20 -0300

    config: using pint

--- app/Http/Requests/areas/AreaCreateRequest.php
+++ app/Http/Requests/areas/AreaCreateRequest.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Requests\areas;
 
-use App\Rules\CondominiumExistRule;
 use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Http\Exceptions\HttpResponseException;
@@ -16,7 +15,7 @@ class AreaCreateRequest extends FormRequest
             'days' => ['required', 'date_format:Y-m-d'],
             'start_time' => ['required', 'date_format:H:i:s'],
             'end_time' => ['required', 'date_format:H:i:s'],
-            'condominium_id' => ['required', 'numeric','condominium_exists'],
+            'condominium_id' => ['required', 'numeric', 'condominium_exists'],
         ];
     }
 
@@ -24,7 +23,7 @@ class AreaCreateRequest extends FormRequest
     {
         throw new HttpResponseException(response()->json([
             'error' => true,
-            'message' => $validator->errors()->first()
+            'message' => $validator->errors()->first(),
         ]));
     }
 }
