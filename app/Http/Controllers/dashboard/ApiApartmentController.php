07d26408a7f1647636e96f66c7a73ffbda688f9d
IgorMarqx <igormarquesdeazevedo11@gmail.com>
2023-11-16 20:33:43 -0300

    config: using pint

--- app/Http/Controllers/dashboard/ApiApartmentController.php
+++ app/Http/Controllers/dashboard/ApiApartmentController.php
@@ -22,6 +22,7 @@ class ApiApartmentController extends Controller
 
     /**
      * Display a listing of the resource.
+     *
      * @throws \Exception
      */
     public function index(): Collection
@@ -29,12 +30,13 @@ class ApiApartmentController extends Controller
         try {
             return $this->apartmentService->getAll();
         } catch (Exception $e) {
-            throw new Exception('Erro ao listar apartamentos:' . $e->getMessage());
+            throw new Exception('Erro ao listar apartamentos:'.$e->getMessage());
         }
     }
 
     /**
      * Store a newly created resource in storage.
+     *
      * @throws \Exception
      */
     public function store(ApartmentCreateRequest $request): ApiResource
@@ -44,64 +46,67 @@ class ApiApartmentController extends Controller
         try {
             return new ApiResource(['error' => false, 'message' => 'Apartamento criado com sucesso'], 201);
         } catch (Exception $e) {
-            throw new Exception('Erro ao criar apartamento:' . $e->getMessage());
+            throw new Exception('Erro ao criar apartamento:'.$e->getMessage());
         }
     }
 
     /**
      * Display the specified resource.
+     *
      * @throws \Exception
      */
     public function show(string $id): ApiResource|ApartmentShowResource
     {
         $apartment = $this->apartmentService->findApartmentById($id);
 
-        if (!$apartment) {
+        if (! $apartment) {
             return new ApiResource(['error' => true, 'message' => 'Apartamento não encontrado'], 422);
         }
 
         try {
             return new ApartmentShowResource($apartment);
         } catch (Exception $e) {
-            throw new Exception('Ocorreu um erro: ' . $e->getMessage());
+            throw new Exception('Ocorreu um erro: '.$e->getMessage());
         }
     }
 
     /**
      * Update the specified resource in storage.
+     *
      * @throws \Exception
      */
     public function update(ApartmentUpdateRequest $request, string $id): ApiResource
     {
         $apartment = $this->apartmentService->updateApartment($request, $id);
 
-        if (!$apartment) {
+        if (! $apartment) {
             return new ApiResource(['error' => true, 'message' => 'Apartamento não encontrado'], 404);
         }
 
         try {
             return new ApiResource(['error' => false, 'message' => 'Apartamento atualizado com sucesso'], 200);
         } catch (Exception $e) {
-            throw new Exception('Ocorreu um erro: ' . $e->getMessage());
+            throw new Exception('Ocorreu um erro: '.$e->getMessage());
         }
     }
 
     /**
      * Remove the specified resource from storage.
+     *
      * @throws \Exception
      */
     public function destroy(string $id): ApiResource
     {
         $apartment = $this->apartmentService->deleteApartment($id);
 
-        if (!$apartment) {
+        if (! $apartment) {
             return new ApiResource(['error' => true, 'message' => 'Apartamento não encontrado'], 422);
         }
 
         try {
             return new ApiResource(['error' => false, 'message' => 'Apartamento deletado com sucesso'], 200);
         } catch (Exception $e) {
-            throw new Exception('Ocorreu um erro: ' . $e->getMessage());
+            throw new Exception('Ocorreu um erro: '.$e->getMessage());
         }
 
     }
