478f8df86eb02f116e4319ad35b070df9b6e3062
IgorMarqx <igormarquesdeazevedo11@gmail.com>
2023-11-16 20:36:08 -0300

    config: using pint

--- app/Repositories/garage/GarageRepositoryInterface.php
+++ app/Repositories/garage/GarageRepositoryInterface.php
@@ -8,7 +8,10 @@ use Illuminate\Database\Eloquent\Collection;
 interface GarageRepositoryInterface
 {
     public function storeGarage($data): Garage;
+
     public function findGarageById($id): Garage|null|Collection;
+
     public function updateGarage(Garage $garage, $data): Garage|bool|null;
+
     public function deleteGarage(Garage $garage): Garage|bool|null;
 }
