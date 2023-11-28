70591e1a31fc2741dfe5c4a357fc759452c65e90
IgorMarqx <igormarquesdeazevedo11@gmail.com>
2023-11-28 18:31:38 -0300

    config: add verifyToken method on repository

--- app/Repositories/auth/AuthRepositoryInterface.php
+++ app/Repositories/auth/AuthRepositoryInterface.php
@@ -3,10 +3,11 @@
 namespace App\Repositories\auth;
 
 use App\Models\User;
+use Illuminate\Database\Eloquent\Collection;
 
 interface AuthRepositoryInterface
 {
     public function getToken($data): string;
-
     public function registerUser($data): User;
+    public function verifyToken($data): Collection;
 }
